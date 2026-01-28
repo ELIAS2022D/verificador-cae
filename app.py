51f8627f1f5708990c5c108302ec7b5416b32d9d
unknown <derricoelias@gmail.com>
2026-01-27 23:03:16 -0300

    adding 1-Planes.py now 27/01/2026 plans nav width

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ st.set_page_config(
 # Header con logo + nombre
 col1, col2 = st.columns([1, 2])
 with col1:
-    st.image("assets/favicon.png", width=300)
+    st.image("assets/favicon.png", width=600)
 with col2:
     st.markdown("## LexaCAE AFIP Validacion en la nube")
     st.caption("## Verificación oficial de CAE contra AFIP (WSCDC)")
